b06f31b85e606944ab3a70654d5fbbaafde9a74c
Mauricio J. Monta <mauriciomontadg@gmail.com>
2022-04-26 04:26:02 -0300

    Working on game

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,7 +1,12 @@
 export default function Game() {
 	return (
 		<div className="game">
-			<h1>Do you want to play?</h1>
+			<div className="firstscreen">
+				<h1>Do you want to play?</h1>
+				<div className="buttons">
+					<button>Sure, go ahead</button>
+				</div>
+			</div>
 		</div>
 	);
 }
